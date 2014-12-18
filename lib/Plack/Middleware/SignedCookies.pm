4eef590bbf8d9c4080b3ca474c272f479fabf8ee
Aristotle Pagaltzis <pagaltzis@gmx.de>
2014-12-18 04:35:17 +0100

    add support for forcing the secure and HttpOnly flags

--- lib/Plack/Middleware/SignedCookies.pm
+++ lib/Plack/Middleware/SignedCookies.pm
@@ -6,7 +6,7 @@ use parent 'Plack::Middleware';
 # ABSTRACT: accept only server-minted cookies
 
 use Plack::Util ();
-use Plack::Util::Accessor qw( secret );
+use Plack::Util::Accessor qw( secret secure httponly );
 use Digest::SHA ();
 
 sub _hmac { y{+/}{-~}, return $_ for Digest::SHA::hmac_sha256_base64( @_[0,1] ) }
@@ -17,7 +17,9 @@ sub call {
 	my $self = shift;
 	my $env  = shift;
 
-	my $secret = $self->secret
+	my $secure   = $self->secure   // do { $self->secure  ( 0 ) };
+	my $httponly = $self->httponly // do { $self->httponly( 1 ) };
+	my $secret   = $self->secret
 		// do { $self->secret( join '', map { chr int rand 256 } 1..17 ) };
 
 	my $cookie =
@@ -33,9 +35,12 @@ sub call {
 	return Plack::Util::response_cb( $self->app->( $env ), sub {
 		my ( $i, $headers ) = ( 0, $_[0][1] );
 		while ( $i < $#$headers ) {
-			'set-cookie' eq lc $headers->[$i++]
-				? $headers->[$i++] =~ s!\A\s*([^;]+?)\K\s*(?=;|\z)!_hmac $1, $secret!e
-				: ++$i;
+			++$i, next if 'set-cookie' ne lc $headers->[$i++];
+			for ( $headers->[$i++] ) {
+				s!\A\s*([^;]+?)\K\s*(?=;|\z)!_hmac $1, $secret!e;
+				$_ .= '; secure'   if $secure   and not /;\s* secure   \s* (?:;|\z)/ix;
+				$_ .= '; HTTPonly' if $httponly and not /;\s* httponly \s* (?:;|\z)/ix;
+			}
 		}
 	} );
 }
@@ -69,6 +74,34 @@ It rejects incoming cookies that were sent without a valid digest.
 The secret to pass to the L<Digest::SHA> HMAC function.
 
 If not provided, a random secret will be generated using PerlE<rsquo>s built-in L<rand> function.
+
+=item C<secure>
+
+Whether to force the I<secure> flag to be set on all cookies,
+which instructs the browser to only send them when using an encrypted connection.
+
+Defaults to false. B<You should strongly consider overriding this default with a true value.>
+
+=item C<httponly>
+
+Whether to force the I<HttpOnly> flag to be set on all cookies,
+which instructs the browser to not make them available to Javascript on the page.
+
+B<Defaults to true.> Provide a defined false value if you wish to override this.
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+L<RFCE<nbsp>6265, I<HTTP State Management Mechanism>, section 4.1.2.5., I<The Secure Attribute>|http://tools.ietf.org/html/rfc6265#section-4.1.2.5>
+
+=item *
+
+L<MSDN, I<Mitigating Cross-site Scripting With HTTP-only Cookies>|http://msdn.microsoft.com/en-us/library/ms533046.aspx>
 
 =back
 
